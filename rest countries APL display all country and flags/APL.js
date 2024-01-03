5a3be94b4cccc00c1b2871113302473bcce54c16
Shanmugamsham <abdeshanmugam@gmail.com>
2024-01-03 22:12:32 +0700

    new

--- rest countries APL display all country and flags/APL.js
+++ rest countries APL display all country and flags/APL.js
@@ -1,13 +1,16 @@
+
+
+
 var request = new XMLHttpRequest();
 request.open("GET","https://restcountries.com/v3.1/all");
 request.send();
 request.onload = function(){
     var result =JSON.parse(request.response);
     console.log(result)
-    for (var i in result){
-        console.log(i,result[i]);
-        // i can only bring all datas
-        // i can't bring result specific data i don't know how to bring result 
-        // i tried manyways please alternate 
-    } 
+    for (var a in result){
+        console.log(result[a].name["common"],result[a].flags["png"]);
+  
+    }
+        
+    
 }
