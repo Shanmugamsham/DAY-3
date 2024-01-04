54275a9bef4fb6c424803af675949b48b94c2bc0
Shanmugamsham <abdeshanmugam@gmail.com>
2024-01-04 14:12:25 +0700

    new

--- rest countries APL display all country and flags/APL.js
+++ rest countries APL display all country and flags/APL.js
@@ -1,19 +1,19 @@
 
 // 2 // county name and flags
 
-//var request = new XMLHttpRequest();
-//request.open("GET","https://restcountries.com/v3.1/all");
-//request.send();
-//request.onload = function(){
-    //var result =JSON.parse(request.response);
-    //console.log(result)
-    //for (var a in result){
-       // console.log(result[a].name["common"],result[a].flags["png"]);
+var request = new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/all");
+request.send();
+request.onload = function(){
+    var result =JSON.parse(request.response);
+    console.log(result)
+    for (var a in result){
+        console.log(result[a].name["common"],result[a].flags["png"]);
   
-    //}
+    }
         
     
-//}
+}
 
 // 3 // counry name, region, sub_region and population
 
